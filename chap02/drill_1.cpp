41c3b8982c8b171cfb82b87e77769f1ee80f073a
k1enn <dinhtrungkien05@gmail.com>
2026-08-17 14:32:09 +0700

    add(chap2): drill1

---
+++ chap02/drill_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+/*
+Write a program that produces a simple form letter based on user input.
+Begin by typing the code from §2.1 prompting a user to enter his or her first\
+name and writing “Hello, first_name” where first_name is the name entered by the user.
+Then modify your code as follows: change the prompt to “Enter the name of the person you want to write to”
+and change the output to “Dear first_name,”. Don’t forget the comma.
+*/
+/*
+int main()
+{
+    std::string first_name = "";
+    std::cout << "Enter your first_name: \n";
+    std::cin >> first_name;
+    std::cout << "Hello, " << first_name;
+}
+*/
+int main()
+{
+    std::string first_name = "";
+    std::cout << "Enter the name of the person you want to write to: \n";
+    std::cin >> first_name;
+    std::cout << "Dear " << first_name << ",";
+}
